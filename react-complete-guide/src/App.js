3553679f20126dffbadcba66de2fa9e0556f91a1
Florent TETAUD <ftetaud@sodifrance.fr>
2019-10-10 14:50:09 +0200

    INIT[] : initialization before starting first assignment

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 29},
+      {name: 'Stephanie', age: 26}
+    ]
+  });
+
+  const [otherState, setOtherState] = useState({
+    otherState: 'some other value'
+  });
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = (newName)=> {
+    setPersonsState({
+      persons: [
+        {name: newName, age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 27}
+    ]});
+  };
+
+  const nameChangedHandler = (event) => {
+    setPersonsState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 26}
+    ]})
+  }
+  
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a react app</h1>
+      <p>This is really working!</p>
+      <button 
+        style={style}
+        onClick={() => switchNameHandler('Maximilian !!')}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age} />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        click={() => switchNameHandler('Max!')}
+        changed={nameChangedHandler}>
+          My Hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age} />
+    </div>
+  );
+}
+
+export default app;
